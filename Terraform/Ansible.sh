215e5a30c951c40dbf538321543e78b615e6f5d9
Vici <svyatoslav12381@gmail.com>
2020-11-28 22:04:23 +0200

    added consent

--- Terraform/Ansible.sh
+++ Terraform/Ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
-apt install apache2
-sudo apt install software-properties-common
+apt install apache2 -y
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 ansible-galaxy collection install community.mysql
