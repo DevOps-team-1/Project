f3968797c10a46519226f335e7cbaecbb7897849
samironius <samirshahubs@gmail.com>
2020-12-03 19:57:29 +0200

    instance for configure (fix)

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -31,3 +31,44 @@ resource "google_compute_firewall" "my_firewall" {
     ports    = ["80", "8080", "1000-2000"]
   }
 }
+////// file edit
+
+resource "local_file" "hosts_cfg" {
+  content = templatefile(file("hosts"), { lamp_instances = google_compute_instance_template.my_lamp_instance.network_interface[0].access_config[0].nat_ip }                             )
+  filename = "hosts_cfg"
+}
+
+//////////another method
+
+//data "template_file" "dev_hosts" {
+//  template = "${file("hosts")}"
+//  vars {
+//    api_public = "${google_compute_instance_template.my_lamp_instance.network_interface[0].access_config[0].nat_ip}"
+//  }
+//}
+//
+//resource "null_resource" "dev-hosts" {
+//  triggers {
+//    template_rendered = "${data.template_file.dev_hosts.rendered}"
+//  }
+//  provisioner "local-exec" {
+//    command = "echo '${data.template_file.dev_hosts.rendered}' > hosts"
+//  }
+//}
+
+
+
+
+//////////another method
+
+//data  "template_file" "k8s" {
+//    template = "${file("hosts")}"
+//    vars {
+//        k8s_master_name = "${join("\n", google_compute_instance_template.my_lamp_instance.network_interface[0].access_config[0].nat_ip)}"
+//    }
+//}
+//
+//resource "local_file" "k8s_file" {
+//  content  = "${data.template_file.k8s.rendered}"
+//  filename = "hosts"
+//}
