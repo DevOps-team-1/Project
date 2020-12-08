5f39cfcbc2424db6baf3a5339ca692e18c658e9d
11daria <45182420+11daria@users.noreply.github.com>
2020-12-08 17:09:24 +0200

    Виправлено помилки

---
+++ Tf_main1/varibles.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "credentials" {
+  default = "prefab-mountain-292413-296112-ac76d2510eff.json"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "project" {
+  default = "prefab-mountain-292413-296112"
+}
